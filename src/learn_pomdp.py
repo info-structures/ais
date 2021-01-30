07bb45697e09d14a7f71c2546ee4ba755d0f4aaf
Lujie Yang <yanglujie@berkeley.edu>
2021-01-29 21:00:14 -0500

    Add Baum Welch algorithm for learning pomdp

--- src/learn_pomdp.py
+++ src/learn_pomdp.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import gym
+import learn_graph as lg
 from train_manager import batch_creator
 
 import torch
@@ -56,110 +57,39 @@ torch.manual_seed(seed)
 env.seed(seed)
 
 
-def collect_sample(bc, greedy=False, n_episodes=1):
-    y = []
-    a = []
-    states = []
-    O = []
-    for n_eps in range(n_episodes):
-
-        if bc.args.env_name[:8] == 'MiniGrid':
-            current_obs = bc.env.reset()['image']
-            current_obs = bc.get_encoded_obs(current_obs)
-        else:
-            next_obs = bc.env.reset()
-            current_obs = bc.convert_int_to_onehot(next_obs, bc.env.observation_space.n)
-        action = torch.zeros(bc.env.action_space.n)
-        reward = 0.
-        hidden = None
-
-
-        obs_history = []
-        action_history = []
-        state_history = []
-        O_history = []
-        for j in range(1000):
-            rho_input = torch.cat((current_obs, action, torch.Tensor([reward]))).reshape(1, 1, -1)
-            ais_z, hidden = bc.rho(rho_input, hidden)
-            ais_z = ais_z.reshape(-1)
-
-            policy_probs = bc.policy(ais_z.detach())
-            if greedy:
-                c = Categorical(policy_probs)
-                _, action = policy_probs.max(0)
-            else:
-                c = Categorical(policy_probs)
-                action = c.sample()
-
-            next_obs, reward, done, _ = bc.env.step(action.item())
-
-            obs_history.append(next_obs)  # + 1 to shift the index starting from 1
-            action_history.append(action.item())
-            O_history.append((reward,next_obs))
-            state_history.append(bc.env.current_state)
-
-            action = bc.convert_int_to_onehot(action, bc.env.action_space.n)
-
-
-            if bc.args.env_name[:8] == 'MiniGrid':
-                current_obs = next_obs['image']
-                current_obs = bc.get_encoded_obs(current_obs)
-            else:
-                current_obs = next_obs
-                current_obs = bc.convert_int_to_onehot(current_obs, bc.env.observation_space.n)
-
-            if done:
-                y.append(obs_history)
-                a.append(action_history)
-                O.append(O_history)
-                states.append(state_history)
-                break
-
-    return y, a, O, states
-
-
 def initialization(y, a, O, n_episodes):
     ny = 7 #len(list(set(y[0])))
     na = 4 #len(list(set(a[0])))
     Ot = []
-    y_a = []
     for r in range(n_episodes):
         Ot += O[r]
-        y_a += list(zip(y[r],a[r]))
     # list of unique (reward, observation) pairs
     Ot = list(set(Ot))
-    y_a = list(set(y_a))
     nO = len(Ot)
 
     # Initialization
     # Transition Probabilities
-    A = np.ones((nz, ny, na, nz))
+    A = np.ones((nz, na, nz))
     A = A / nz
 
-    # Using prior knowledge about (y,a) pairs to update A
-    # for j in range(ny):
-    #     for k in range(na):
-    #         if (j,k) not in y_a:
-    #             A[:,j,k,:] = np.zeros((nz,nz))
-
     # Emission Probabilities
-    B = np.random.random((nz, na, nO))
-    # np.sum(B, axis=2)
+    B = np.random.random((nz, ny))
+    B = B / np.sum(B, axis=1).reshape((-1, 1))
 
     initial_distribution = np.ones(nz)/nz
 
-    return A, B, initial_distribution, ny, na, nz, nO, Ot, y_a
+    return A, B, initial_distribution, ny, na, nz, nO, Ot
 
 
-def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a, n_episodes, n_iter=100, epsilon=1e-8):
+def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, n_episodes, n_iter=100, epsilon=1e-8):
     for n in range(n_iter):
         print('Iteration: ', n)
         if n%10 == 0 and args.save_graph:
-            save_graph(A, B, initial_distribution, Ot, y_a, nz, seed)
-        A_num = np.zeros((nz, ny, na, nz))
-        A_den = np.zeros((nz, ny, na))
-        B_num = np.zeros((nz, na, nO))
-        B_den = np.zeros((nz, na))
+            save_graph(A, B, initial_distribution, Ot, nz, seed)
+        A_num = np.zeros((nz, na, nz))
+        A_den = np.zeros((nz, na))
+        B_num = np.zeros((nz, ny))
+        B_den = np.zeros(nz)
         initial_distribution_num = np.zeros(nz)
         R = n_episodes
         for r in range(n_episodes):
@@ -177,7 +107,7 @@ def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a, n_e
                 for t in range(T-1):
                     denominator = np.dot(alpha[:, t], beta[:, t])
                     for i in range(nz):
-                        numerator = alpha[i, t] * A[i, yr[t], ar[t], :] * B[:, ar[t+1], Ot.index(Or[t+1])] * beta[:, t+1]
+                        numerator = alpha[i, t] * A[i, ar[t], :] * B[:, yr[t+1]] * beta[:, t+1]
                         if denominator == 0:
                             xi[i, :, t] = np.zeros(nz)
                         else:
@@ -187,10 +117,10 @@ def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a, n_e
 
                 initial_distribution_num += gamma[:,0]
 
-                for j in range(ny):
-                    for k in range(na):
-                        A_num[:,j,k,:] += np.sum(xi[:,:,(yr[0:T-1]==j)*(ar[0:T-1]==k)],axis=2)
-                        A_den[:,j,k] += np.sum(gamma[:,(yr[0:T-1]==j)*(ar[0:T-1]==k)], axis=1)
+
+                for k in range(na):
+                    A_num[:,k,:] += np.sum(xi[:,:,(ar[0:T-1]==k)],axis=2)
+                    A_den[:,k] += np.sum(gamma[:,(ar[0:T-1]==k)], axis=1)
 
                 # Add gamma at T
                 if np.dot(alpha[:, -1], beta[:, -1]) ==0:
@@ -199,36 +129,29 @@ def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a, n_e
                     gammaT =  np.array(alpha[:,-1]*beta[:,-1]/np.dot(alpha[:, -1], beta[:, -1])).reshape((-1,1))
                 gamma = np.hstack((gamma, gammaT))
 
-                for j in range(na):
-                    for k in range(nO):
-                        match_O = np.array(Or)==np.array(Ot)[k]
-                        ind_O = match_O[:,0]*match_O[:,1]
-                        B_num[:, j, k] += np.sum(gamma[:,(ar==j)*(ind_O)], axis=1)
-                        B_den[:, j] += np.sum(gamma[:,ar==j], axis=1)
+                for j in range(ny):
+                    B_num[:, j] += np.sum(gamma[:,(yr==j)], axis=1)
+                    B_den[:] += np.sum(gamma, axis=1)
 
-
-        for i in range(nz):
-            for j in range(ny):
-                for k in range(na):
-                    for l in range(nz):
-                        if A_den[i,j,k]<epsilon:
-                            A[i, j, k, l] = 0
-                        else:
-                            A[i,j,k,l] = A_num[i,j,k,l]/A_den[i,j,k]
-        # Check marginalization
-        # assert ((np.absolute(np.sum(A,axis=3)- np.ones((nz, ny, na)))< epsilon).all())
 
         for i in range(nz):
             for j in range(na):
-                for k in range(nO):
-                    if B_den[i,j] < epsilon:
-                        B[i, j, k] = 0
+                for k in range(nz):
+                    if A_den[i, j]<epsilon:
+                        A[i, j, k] = 0
                     else:
-                        B[i,j,k] = B_num[i,j,k]/B_den[i,j]
+                        A[i,j,k] = A_num[i,j,k]/A_den[i,j]
         # Check marginalization
-        # assert ((np.absolute(np.sum(B, axis=2) - np.ones((nz, na))) < epsilon).all())
-        # Re-normalize
-        B = B/np.sum(B, axis=2)[0,0]
+        # assert ((np.absolute(np.sum(A,axis=2)- np.ones((nz, ny, na)))< epsilon).all())
+
+        for i in range(nz):
+            for j in range(ny):
+                    if B_den[i] < epsilon:
+                        B[i, j] = 0
+                    else:
+                        B[i, j] = B_num[i,j]/B_den[i]
+        # Check marginalization
+        # assert ((np.absolute(np.sum(B, axis=1) - np.ones((nz, na))) < epsilon).all())
 
         initial_distribution = initial_distribution_num/R
 
@@ -238,11 +161,11 @@ def baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a, n_e
 def forward(A, B, initial_distribution, nz, y, a, O, Ot):
     T = len(y)
     alpha = np.zeros((nz, T))
-    alpha[:,0] = initial_distribution * B[:, a[0], Ot.index(O[0])]
+    alpha[:,0] = initial_distribution * B[:, y[0]]
 
     for t in range(1,T):
         for j in range(nz):
-            alpha[j,t] = alpha[:,t-1].dot(A[:,y[t-1],a[t-1],j])*B[j,a[t],Ot.index(O[t])]
+            alpha[j,t] = alpha[:,t-1].dot(A[:,a[t-1],j])*B[j,y[t]]
 
     return alpha
 
@@ -254,16 +177,15 @@ def backward(A, B, nz, y, a, O, Ot):
 
     for t in range(T-2,-1,-1):
         for j in range(nz):
-            beta[j,t] = (beta[:,t+1]*B[:,a[t+1],Ot.index(O[t+1])]).dot(A[j,y[t],a[t],:])
+            beta[j, t] = (beta[:, t+1]*B[:, y[t+1]]).dot(A[j, a[t], :])
 
     return beta
 
 
-def minimize(A, B, Ot, nz, y_a, epsilon=1e-8):
-    goal_obs = (1,6)
-    goal_obs_ind = to_tuple(Ot).index(goal_obs)
+def minimize(A, B, Ot, nz, na, epsilon=1e-8):
+    goal_obs = 6
 
-    goal_ind = np.any(np.absolute(B[:,:,goal_obs_ind]-1) < epsilon, axis=1)
+    goal_ind = np.any(np.absolute(B[:,goal_obs]-1) < epsilon, axis=1)
     z2goal = np.arange(nz)[goal_ind]
 
     z = list(range(nz))
@@ -376,120 +298,93 @@ def observation_count(y):
         print("Obs {} appear {}".format(key+1, dict[key]/num_total_obs))
 
 
-def plot_A(A,y_a, obs=None, Ar=None):
+def plot_A(A,na, obs=None, Ar=None):
     """
     obs: if not None, only plot A with observation y=obs; else plot all A's
     Ar: if not None, plot A with reduced Ar for comparison
     """
-
-    if obs is None:
-        if Ar is None:
-            for i in y_a:
-                plt.imshow(A[:,i[0],i[1],:])
-                plt.title('P(z(t+1)|z(t)) with y: {}, a: {}'.format(i[0]+1, action_dict[i[1]]))
-                plt.xlabel("z(t+1)")
-                plt.ylabel("z(t)")
-                plt.colorbar()
-                plt.show()
-        else:
-            for i in y_a:
-                plt.subplot(1,2,1)
-                plt.imshow(A[:,i[0],i[1],:])
-                plt.title('P(z(t+1)|z(t)) with y: {}, a: {}'.format(i[0]+1, action_dict[i[1]]))
-                plt.xlabel("z(t+1)")
-                plt.ylabel("z(t)")
-                plt.colorbar()
-                plt.subplot(1, 2, 2)
-                plt.imshow(Ar[:, i[0], i[1], :])
-                plt.title('P(z(t+1)|z(t)) with y: {}, a: {}, reduced A'.format(i[0] + 1, action_dict[i[1]]))
-                plt.xlabel("z(t+1)")
-                plt.ylabel("z(t)")
-                plt.colorbar()
-                plt.show()
-    else:
-        for i in y_a:
-            if i[0]==obs:
-                plt.imshow(A[:,i[0],i[1],:])
-                plt.title('P(z(t+1)|z(t),y(t),a(t)) with y: {}, a: {}'.format(i[0]+1, action_dict[i[1]]))
-                plt.xlabel("z(t+1)")
-                plt.ylabel("z(t)")
-                plt.colorbar()
-                plt.show()
-
-
-def plot_B(B,nz, Ot, state=None):
-    """
-    state: if not None, plot the B transition probability only for z = state
-    """
-    print("Order of Ot (r,y): ", Ot)
-    if state is None:
-        for i in range(nz):
-            plt.imshow(B[i,:,:])
-            plt.title("P(O(t)|z(t),a(t)) with z{}".format(i))
-            plt.xlabel("O(t) index")
-            plt.ylabel("a(t)")
+    if Ar is None:
+        for i in range(na):
+            plt.imshow(A[:,i,:])
+            plt.title('P(z(t+1)|z(t), a(t)) with a: {}'.format(action_dict[i]))
+            plt.xlabel("z(t+1)")
+            plt.ylabel("z(t)")
             plt.colorbar()
             plt.show()
     else:
-        plt.imshow(B[state, :, :])
-        plt.title("P(O(t)|z(t),a(t)) with z{}".format(state))
-        plt.xlabel("O(t) index")
-        plt.ylabel("a(t)")
-        plt.colorbar()
-        plt.show()
+        for i in range(a):
+            plt.subplot(1,2,1)
+            plt.imshow(A[:, i, :])
+            plt.title('P(z(t+1)|z(t), a(t)) with a: {}'.format(action_dict[i]))
+            plt.xlabel("z(t+1)")
+            plt.ylabel("z(t)")
+            plt.colorbar()
+            plt.subplot(1, 2, 2)
+            plt.imshow(Ar[:, i, :])
+            plt.title('P(z(t+1)|z(t), a(t)) with a: {}, reduced A'.format(action_dict[i]))
+            plt.xlabel("z(t+1)")
+            plt.ylabel("z(t)")
+            plt.colorbar()
+            plt.show()
 
 
-def save_graph(A, B, initial_distribution, Ot, y_a, nz, seed):
+
+def plot_B(B):
+    plt.imshow(B)
+    plt.title("P(y(t)|z(t)")
+    plt.xlabel("y(t)")
+    plt.ylabel("z(t)")
+    plt.colorbar()
+    plt.show()
+
+
+def save_graph(A, B, initial_distribution, Ot, nz, seed):
     if args.load_policy:
-        np.save("graph/optimal_policy/A_{}_{}".format(nz, seed), A)
-        np.save("graph/optimal_policy/B_{}_{}".format(nz, seed), B)
-        np.save("graph/optimal_policy/initial_distribution_{}_{}".format(nz, seed), initial_distribution)
-        np.save("graph/optimal_policy/Ot_{}_{}".format(nz, seed), Ot)
+        np.save("graph/pomdp/optimal_policy/A_{}_{}".format(nz, seed), A)
+        np.save("graph/pomdp/optimal_policy/B_{}_{}".format(nz, seed), B)
+        np.save("graph/pomdp/optimal_policy/initial_distribution_{}_{}".format(nz, seed), initial_distribution)
+        np.save("graph/pomdp/optimal_policy/Ot_{}_{}".format(nz, seed), Ot)
     else:
-        np.save("graph/A_{}_{}".format(nz, seed), A)
-        np.save("graph/B_{}_{}".format(nz, seed), B)
-        np.save("graph/initial_distribution_{}_{}".format(nz, seed), initial_distribution)
-        np.save("graph/Ot_{}_{}".format(nz, seed), Ot)
-        # np.save("graph/y_a", y_a)
+        np.save("graph/pomdp/A_{}_{}".format(nz, seed), A)
+        np.save("graph/pomdp/B_{}_{}".format(nz, seed), B)
+        np.save("graph/pomdp/initial_distribution_{}_{}".format(nz, seed), initial_distribution)
+        np.save("graph/pomdp/Ot_{}_{}".format(nz, seed), Ot)
 
 
 def load(nz, seed, args):
     if args.load_policy:
-        A = np.load("graph/optimal_policy/A_{}_{}.npy".format(nz, seed))
-        B = np.load("graph/optimal_policy/B_{}_{}.npy".format(nz, seed))
-        initial_distribution = np.load("graph/optimal_policy/initial_distribution_{}_{}.npy".format(nz, seed))
-        Ot = np.load("graph/optimal_policy/Ot_{}_{}.npy".format(nz, seed))
+        A = np.load("graph/pomdp/optimal_policy/A_{}_{}.npy".format(nz, seed))
+        B = np.load("graph/pomdp/optimal_policy/B_{}_{}.npy".format(nz, seed))
+        initial_distribution = np.load("graph/pomdp/optimal_policy/initial_distribution_{}_{}.npy".format(nz, seed))
     else:
-        A = np.load("graph/A_{}_{}.npy".format(nz, seed))
-        B = np.load("graph/B_{}_{}.npy".format(nz, seed))
-        initial_distribution = np.load("graph/initial_distribution_{}_{}.npy".format(nz, seed))
-        Ot = np.load("graph/Ot_{}_{}.npy".format(nz, seed))
-    y_a = np.load("graph/y_a.npy")
-    return A, B, initial_distribution, Ot, y_a
+        A = np.load("graph/pomdp/A_{}_{}.npy".format(nz, seed))
+        B = np.load("graph/pomdp/B_{}_{}.npy".format(nz, seed))
+        initial_distribution = np.load("graph/pomdp/initial_distribution_{}_{}.npy".format(nz, seed))
+    return A, B, initial_distribution
 
 
 if __name__ == "__main__":
     bc = batch_creator(args, env, None, True)
-    nz = 20
+    nz = 11
     action_dict = {0: "N", 1: "S", 2: "E", 3: "W"}
     epsilon = 1e-8
     if args.load_graph:
-        A, B, initial_distribution, Ot, y_a = load(nz, seed, args)
+        A, B, initial_distribution, Ot = load(nz, seed, args)
     else:
         if args.load_policy:
             bc.load_models_from_folder(args.models_folder)
-        y, a, O, states = collect_sample(bc, greedy=False, n_episodes=N_eps_eval)
-        A, B, initial_distribution, ny, na, nz, nO, Ot, y_a = initialization(y, a, O, N_eps_eval)
-        A, B, initial_distribution = baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot, y_a,
+        y, a, O, states = lg.collect_sample(bc, greedy=False, n_episodes=N_eps_eval)
+        A, B, initial_distribution, ny, na, nz, nO, Ot = initialization(y, a, O, N_eps_eval)
+        A, B, initial_distribution = baum_welch(y, a, O, A, B, initial_distribution, ny, na, nz, nO, Ot,
                                                 N_eps_eval, 100, epsilon)
-        partition = minimize(A, B, Ot, nz, y_a, epsilon)
-        Ar, Br, nzr = reduce_A_B(A, B, partition)
-        Ar, Br, initial_distribution = baum_welch(y, a, O, Ar, Br, initial_distribution, ny, na, nzr, nO, Ot, y_a,
-                                                N_eps_eval, 50, epsilon)
+        # partition = minimize(A, B, Ot, nz, a, epsilon)
+        # Ar, Br, nzr = reduce_A_B(A, B, partition)
+        # Ar, Br, initial_distribution = baum_welch(y, a, O, Ar, Br, initial_distribution, ny, na, nzr, nO, Ot,
+        #                                         N_eps_eval, 50, epsilon)
         if args.save_graph:
-            save_graph(A, B, initial_distribution, Ot, y_a, nz, seed)
-    plot_A(A, y_a, Ar=Ar)
-    plot_B(B, nz, Ot)
+            save_graph(A, B, initial_distribution, Ot, nz, seed)
+    plot_A(A, na)
+    plot_B(B)
 
 
 
