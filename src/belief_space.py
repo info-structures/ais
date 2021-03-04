cb9a8f9e68c8589da0cb879397300f48bbc8cb31
Lujie Yang <yanglujie@berkeley.edu>
2021-03-04 10:30:54 -0500

    Add deterministic transition matching

--- src/belief_space.py
+++ src/belief_space.py
@@ -54,7 +54,7 @@ nb = b.shape[0]
 
 C = np.zeros((nb, nb, nu))
 R = np.zeros((nb, nu))
-
+# Find reward for each belief
 for r in range(len(y)):
     if r % 10 == 0:
         print("r ", r)
@@ -64,23 +64,50 @@ for r in range(len(y)):
     bn = b0
     for t in range(len(yr)):
         # Probability (b_{n+1}|b_n, u_n)
-        Pbb = np.zeros(nb)
-        for i in range(nb):
-            for yb in range(ny):
-                Py = D[yb]@P_xu[ar[t]]@bn
-                if np.array_equal(b[i, :], Py / np.sum(Py)):
-                    Pbb[i] += np.diag(D[yb])@P_xu[ar[t]]@bn
+        # Pbb = np.zeros(nb)
+        # for i in range(nb):
+        #     for yb in range(ny):
+        #         Py = D[yb]@P_xu[ar[t]]@bn
+        #         if np.array_equal(b[i, :], Py / np.sum(Py)):
+        #             Pbb[i] += np.diag(D[yb])@P_xu[ar[t]]@bn
         if bn in b:
             ind = np.where((bn == b).all(axis=1))[0][0]
             R[ind, ar[t]] = Or[t][0]
-            C[:, ind, ar[t]] = Pbb
+            # C[:, ind, ar[t]] = Pbb
 
         bn = D[yr[t]]@P_xu[ar[t]]@bn
         bn = bn / np.sum(bn, axis=0)
 
+# Calculate probability (b_{n+1}|b_n, u_n)
+for j in range(nb):
+    bn = b[j, :]
+    for u in range(nu):
+        Pbb = np.zeros(nb)
+        for i in range(nb):
+            for yb in range(ny):
+                Py = D[yb]@P_xu[u]@bn
+                if np.array_equal(b[i, :], Py / np.sum(Py)):
+                    Pbb[i] += np.diag(D[yb])@P_xu[u]@bn
+        C[:, j, u] = Pbb
+
+# Calculate deterministic transition b_{n+1}= C_det(b_n, u_n, y_n)
+n_ya = len(y_a)
+C_det = np.zeros((nb, nb, n_ya))
+for j in range(nb):
+    bn = b[j, :]
+    for k in range(n_ya):
+        b_next = D[y_a[k][0]]@P_xu[y_a[k][1]]@bn
+        b_next = b_next / np.sum(b_next, axis=0)
+        for i in range(nb):
+            if np.array_equal(b[i, :], b_next):
+                C_det[i, j, k] = 1
+                break
+
+
 np.save("src/b", b)
 # np.save("src/C", C)
 # np.save("src/R", R)
+np.save("src/C_det", C_det)
 
 
 
--- src/exact_reduction.py
+++ src/exact_reduction.py
@@ -10,6 +10,7 @@ from gurobipy import GRB
 parser = argparse.ArgumentParser(description='This code runs AIS using the Next Observation prediction version')
 parser.add_argument("--save_graph", help="Save the transition probabilities", action="store_true")
 parser.add_argument("--load_graph", help="Load the transition probabilities", action="store_true")
+parser.add_argument("--AIS_state_size", type=int, help="Load the transition probabilities", default=11)
 args = parser.parse_args()
 
 
@@ -36,44 +37,61 @@ def runGUROBIImpl(n, theta=0.5, nIter=10):
 
                 if not (model.status == GRB.OPTIMAL): print("unsuccessful...")
 
-def runCVXPYImpl(nz, nb, nu, C, R):
+def runCVXPYImpl(nz, nb, nu, C, R, C_det=None, P_y_b_u=None):
     Q1 = cp.Variable((nz, nb), nonneg=True)
     Q2 = cp.Variable((nz, nb), nonneg=True)
     Q3 = cp.Variable((nz, nb), nonneg=True)
     Q4 = cp.Variable((nz, nb), nonneg=True)
     Q = [Q1, Q2, Q3, Q4]
     D = cp.Variable((nz, nb), boolean=True)
-    r = cp.Variable((nb, nu))
+    r_bar = cp.Variable((nb, nu))
+    Q_det = []
+    P_y_z_u_bar = []
     loss = 0
     constraints = []
-    for i in range(nu):
-        for j in range(nb):
-            b_one_hot = np.zeros(nb)
-            b_one_hot[j] = 1
+    for j in range(nb):
+        b_one_hot = np.zeros(nb)
+        b_one_hot[j] = 1
+        for i in range(nu):
             # Match transition distributions
             loss += cp.norm(cp.matmul(Q[i], b_one_hot)-cp.matmul(D, C[:, :, i]@b_one_hot))
             # Match reward
-            loss += cp.norm(R[j, i] - cp.matmul(r[:, i], b_one_hot))
+            loss += cp.norm(R[j, i] - cp.matmul(r_bar[:, i], b_one_hot))
             constraints += [cp.sum(D) == nz,
                             cp.matmul(np.ones((1, nz)), D) <= 1,
                             cp.matmul(np.ones((1, nz)), D) == cp.matmul(np.ones((1, nz)), Q[i]),
                             cp.matmul(D, np.ones((nb, 1))) == np.ones((nz, 1)), ]
 
+            if P_y_b_u is not None:
+                P_y_z_u_bar.append(cp.Variable(P_y_b_u.shape[0], nb))
+                loss += cp.norm(cp.matmul(P_y_z_u_bar[i], b_one_hot) - P_y_b_u[:, :, i]@ b_one_hot)
+                constraints += [cp.matmul(np.ones((1, nz)), D) == cp.matmul(np.ones((1, nz)), P_y_z_u_bar[i]), ]
+
+        if C_det is not None:
+            for k in range(C_det.shape[2]):
+                Q_det.append(cp.Variable((nz, nb), boolean=True))
+                loss += cp.norm(cp.matmul(Q_det[k], b_one_hot) - cp.matmul(D, C_det[:, :, k] @ b_one_hot))
+                # B_det is also part of permutation matrix
+                constraints += [cp.matmul(np.ones((1, nz)), D) == cp.matmul(np.ones((1, nz)), Q_det[k]), ]
 
     objective = cp.Minimize(loss)
-
     problem = cp.Problem(objective, constraints)
 
-
     # solve problem
-    # problem.solve(solver=cp.GUROBI, verbose=False)
     problem.solve(solver=cp.GUROBI, verbose=False)
 
     if not (problem.status == cp.OPTIMAL):
         print("unsuccessful...")
     else:
         print("loss ", loss.value)
-    return np.array([Q1.value, Q2.value, Q3.value, Q4.value]), D.value, r.value
+
+    if C_det is not None:
+        Q_det_out = []
+        for i in range(len(Q_det)):
+            Q_det_out.append(Q_det[i].value)
+        return np.array(Q_det_out), np.array([Q1.value, Q2.value, Q3.value, Q4.value]), D.value, r_bar.value
+    else:
+        return np.array([Q1.value, Q2.value, Q3.value, Q4.value]), D.value, r_bar.value
 
 
 def value_iteration(B, r, nz, na, epsilon=0.0001, discount_factor=0.95):
@@ -96,9 +114,10 @@ def value_iteration(B, r, nz, na, epsilon=0.0001, discount_factor=0.95):
         z_one_hot = np.zeros(nz)
         z_one_hot[z] = 1
         z_next = B[a, :, :]@z_one_hot
-        # v = np.sum(prob_O * (Ot[:,0] + discount_factor * prob_z@V))
+        # ind = (z_next > epsilon)
+        v = r[a, :]@z_next + discount_factor * z_next@V
 
-        return 0
+        return v
 
     # start with initial value function and initial policy
     V = np.zeros(nz)
@@ -145,26 +164,44 @@ def value_iteration(B, r, nz, na, epsilon=0.0001, discount_factor=0.95):
     return policy, V
 
 
-def eval_performance(policy, A, n_episodes=100, epsilon=1e-8, beta=0.95):
+def eval_performance(policy, D, C_det, V, V_b, y_a, na, nb, B_det=None, n_episodes=100, epsilon=1e-8, beta=0.95):
     returns = []
+    Vs = []
+    V_bs = []
     for n_eps in range(n_episodes):
         reward_episode = []
         y = lg.env.reset()
-        while True:
-            # sample z from initial distribution
-            z = np.where(np.random.multinomial(1,initial_distribution)==1)[0][0]
-            # check z agrees with the first observation
-            if (A[:,y,:, z]>epsilon).any():
-                break
 
+        uniform_distribution = np.ones(nb) / nb
+        while True:
+            # sample b from initial distribution
+            ind_b = np.where(np.random.multinomial(1, uniform_distribution) == 1)[0][0]
+            # check b agrees with the first observation
+            if (C_det[ind_b, :, y == y_a[:, 0]] > epsilon).any():
+                b_one_hot = np.zeros(nb)
+                b_one_hot[ind_b] = 1
+                break
+        z_one_hot = D@b_one_hot
 
         for j in range(1000):
-            action = np.arange(na)[policy[z].astype(bool)][0]
+            try:
+                ind_z = np.where(z_one_hot == 1)[0][0]
+            except:
+                print("No corresponding z")
+            Vs.append(V[ind_z])
+            V_bs.append(V_b[b_one_hot == 1])
+
+            action = np.arange(na)[policy[ind_z].astype(bool)][0]
 
             y, reward, done, _ = lg.env.step(action)
             reward_episode.append(reward)
 
-            z = np.where(np.random.multinomial(1, A[z, y, action, :]) == 1)[0][0]
+            ind_ya = np.where((y == y_a[:, 0])*(action == y_a[:, 1]))[0][0]
+            b_one_hot = C_det[:, :, ind_ya]@b_one_hot
+            if B_det is not None:
+                z_one_hot = B_det@z_one_hot
+            else:
+                z_one_hot = D@b_one_hot
 
             if done:
                 break
@@ -176,39 +213,54 @@ def eval_performance(policy, A, n_episodes=100, epsilon=1e-8, beta=0.95):
             rets.insert(0, R)
         returns.append(rets[0])
 
-    return np.mean(returns)
+    average_return = np.mean(returns)
+    V_mse = np.norm(np.array(Vs)-np.array(V_bs))
+    print("Average reward: ", )
+    print("V mse: ", V_mse)
+    return average_return, V_mse
 
 
-def save_reduction_graph(Q, D, r, nz):
+def save_reduction_graph(Q, D, r_bar, nz, Q_det=None):
     np.save("src/Q_{}".format(nz), Q)
     np.save("src/D_{}".format(nz), D)
-    np.save("src/r_fit_{}".format(nz), r)
+    np.save("src/r_fit_{}".format(nz), r_bar)
+    if Q_det is not None:
+        np.save("src/Q_det_{}".format(nz), Q_det)
 
 
-def load_reduction_graph(nz):
+def load_reduction_graph(nz, det=False):
     Q = np.load("src/Q_{}.npy".format(nz))
     D = np.load("src/D_{}.npy".format(nz))
-    r = np.load("src/r_fit_{}.npy".format(nz))
-    return Q, D, r
+    r_bar = np.load("src/r_fit_{}.npy".format(nz))
+    if det:
+        Q_det = np.load("src/Q_det{}.npy".format(nz))
+        return Q_det, Q, D, r_bar
+    else:
+        return Q, D, r_bar
 
 
 if __name__ == "__main__":
     np.random.seed(0)
-    nz = 11
+    nz = lg.args.AIS_state_size
     nb = 15
     nu = 4
     C = np.load("src/C.npy")
+    C_det = np.load("src/C_det.npy")
     R = np.load("src/R.npy")
+    y_a = np.load("graph/y_a.npy")
 
     if args.load_graph:
-        Q, D, r = load_reduction_graph(nz)
+        Q, D, r_bar = load_reduction_graph(nz)
     else:
-        Q, D, r = runCVXPYImpl(nz, nb, nu, C, R)
+        Q_det, Q, D, r_bar = runCVXPYImpl(nz, nb, nu, C, R, C_det)
         if args.save_graph:
-            save_reduction_graph(Q, D, r, nz)
+            save_reduction_graph(Q, D, r_bar, nz, Q_det)
 
     B = Q@D.T
-    value_iteration(B, r, nz, nu)
+    r = r_bar.T@D.T
+    policy, V = value_iteration(B, r, nz, nu)
+    policy_b, V_b = value_iteration(np.einsum('ijk->kij', C), R.T, nb, nu)
+    eval_performance(policy, D, C_det, V, V_b, y_a, nu, nb)
 
 
 
